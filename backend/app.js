609a900cd2d49ce63801ed78bb81154b73c82010
Neha Ravindra <neharavindra2408@gmail.com>
2025-12-08 17:17:23 +0530

    Backend ready for Render deployment

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
